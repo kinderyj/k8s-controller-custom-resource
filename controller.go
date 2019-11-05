876825a41a35fb5d31fefc9a7f3b2e6cd469f94c
wangyujun <wangyujun@huya.com>
2019-11-05 17:33:44 +0800

    a

--- controller.go
+++ controller.go
@@ -233,11 +233,11 @@ func (c *Controller) syncHandler(key string) error {
 	}
 
 	// Get the Node resource with this node name
-	nodes, err := c.nodeLister.Get(key)
+	node, err := c.nodeLister.Get(key)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			glog.Warningf("[Node]: %s does not exist in local cache, will delete it from whitelist", name)
-			glog.Infof("[Node]: Deleting Node from whitelist: %s ...", name)
+			glog.Warningf("[Node]: %s does not exist in local cache, will delete it from whitelist", node)
+			glog.Infof("[Node]: Deleting Node from whitelist: %s ...", node)
 
 			// FIX ME: call os API to delete this node by name.
 			//
